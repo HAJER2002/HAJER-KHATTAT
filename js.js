d278eb584f32ba085c90dd6800f73bb052c04770
HAJER2002 <140167204+HAJER2002@users.noreply.github.com>
2023-08-03 01:09:07 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,95 @@
+function myFunction() {
+    var x = document.getElementById("nv1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+function Message(){
+  var x = document.getElementById("msgE");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function ovrirM() {
+  var x = document.getElementById("modaleshow");
+ 
+    x.style.display = "block";
+ 
+}
+
+function ovrirM2() {
+  var x = document.getElementById("modaleshow2");
+ 
+    x.style.display = "block";
+ 
+}
+
+function ovrirM3() {
+  var x = document.getElementById("modaleshow3");
+ 
+    x.style.display = "block";
+ 
+}
+
+function ovrirM4() {
+  var x = document.getElementById("modaleshow4");
+ 
+    x.style.display = "block";
+ 
+}
+function ovrirM5() {
+  var x = document.getElementById("modaleshow5");
+ 
+    x.style.display = "block";
+ 
+}
+
+function FirmeM() {
+  var x = document.getElementById("modaleshow");
+ 
+    x.style.display = "none";
+ 
+}
+
+
+function FirmeM2() {
+  var x = document.getElementById("modaleshow2");
+ 
+    x.style.display = "none";
+ 
+}
+function FirmeM3() {
+  var x = document.getElementById("modaleshow3");
+ 
+    x.style.display = "none";
+ 
+}
+
+
+function FirmeM4() {
+  var x = document.getElementById("modaleshow4");
+ 
+    x.style.display = "none";
+ 
+}
+
+function FirmeM5() {
+  var x = document.getElementById("modaleshow5");
+ 
+    x.style.display = "none";
+ 
+}
+
+
+
+
+
+
